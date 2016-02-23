b9dc50c8d56503106cbc9649c20ae4ea51a78a45
Marcus Lilja <marcus.lilja@gmail.com>
2016-02-23 08:47:36 +0100

    Update font task

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,13 +1,13 @@
 
-var gulp = require('gulp'),
-  changed = require('gulp-changed');
+import gulp from 'gulp';
+import changed from 'gulp-changed';
 
-var utils = require('../utils'),
-  config = require('../config');
+import config from '../config';
+import { handleError } from '../utils';
 
-gulp.task('fonts', function () {
+gulp.task('fonts', () => {
   return gulp.src(config.fonts.src)
     .pipe(changed(config.fonts.dest))
     .pipe(gulp.dest(config.fonts.dest))
-    .on('error', utils.handleError);
+    .on('error', handleError);
 });
